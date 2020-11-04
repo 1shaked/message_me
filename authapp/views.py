30b2e8ff5f80693a1776adaa5901d94d57e2f2b5
shaked chen <shaked1hen@gmail.com>
2020-11-04 13:23:06 +0200

    added the filter to delete

--- authapp/views.py
+++ authapp/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 from rest_framework import authentication, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
 
 from django.contrib.auth import get_user_model
 
-from .serializers import UserSerializer, ChatConversionSerializer
+from .serializers import UserSerializer, ChatConversionSerializer, ChatConversionUpdate
 from .models import ChatConversion
 
 # this class base views will create a user on post
@@ -31,3 +34,22 @@ class ChatConversionView(generics.ListCreateAPIView):
         chat_convertion.update(is_read=True) # after every request the is read will change to true
         return chat_convertion
 
+class ChatMessage(generics.RetrieveDestroyAPIView):
+    queryset = ChatConversion.objects.all()
+    serializer_class  = ChatConversionSerializer
+
+    def delete(self, request, *args, **kwargs):
+        msg_id  = kwargs['pk']
+        user_id = kwargs['user_id']
+        # filter by msg id and user_id == sender or recevier
+        chat_msg = ChatConversion.objects.filter(Q(pk=msg_id) ,Q(sender = user_id) | Q(receiver = user_id)  )
+        delete_status = chat_msg.delete()
+        if delete_status[0]:
+            return Response({'deleted' : 'deleted well done'} , status=status.HTTP_204_NO_CONTENT)
+        return Response({'deleted' : 'no object to delete'},  status=status.HTTP_204_NO_CONTENT)
+
+class ChatMessageUpdate(generics.UpdateAPIView):
+    queryset = ChatConversion.objects.all()
+    serializer_class  = ChatConversionUpdate
+
+    
