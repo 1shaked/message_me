7e5b630d9c9d98933359d6ccd23a4b6db9dae8d2
shaked chen <shaked1hen@gmail.com>
2020-11-04 11:30:08 +0200

    init the serializers

--- authapp/serializers.py
+++ authapp/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
-from auth.models import ChatConversion 
+from .models import ChatConversion 
+from django.contrib.auth import get_user_model
 
-class UserCreateSerializer(serializers.ModelSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = settings.AUTH_USER_MODEL
-        fields = '__all__'
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        password = serializers.CharField(
+            min_length=4,
+            write_only=True,
+            required=True,
+            style={'input_type': 'password'}
+        )
 
+        fields = ['email', 'username', 'password',]
+    def create(self, validated_data):
+        print('create user')
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class ChatConversionSerializer(serializers.ModelSerializer):
     class Meta:
