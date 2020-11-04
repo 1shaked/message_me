075c9032fcb7dec7be5206810a7a6a83af3189f5
shaked chen <shaked1hen@gmail.com>
2020-11-04 11:30:37 +0200

    create the views create user and Chat converstion

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from rest_framework import authentication, permissions
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer, ChatConversionSerializer
+from .models import ChatConversion
+
+# this class base views will create a user on post
+class CreateUserView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class ChatConversionView(generics.ListCreateAPIView):
+    queryset = ChatConversion.objects.all()
+    serializer_class = ChatConversionSerializer
+    # permission_classes = [permissions.IsAuthenticated] will be added to auth check at the end
+
+    def get_queryset(self):
+        sender = self.request.query_params.get('sender', None)
+        print(self.request.query_params)
+        print(self.request.query_params)
+        return ChatConversion.objects.filter(sender = sender)
+
