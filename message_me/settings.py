953c92e2ae67256d957912839e20c24f19f10f66
shaked chen <shaked1hen@gmail.com>
2020-11-05 10:18:55 +0200

    added the static url

--- message_me/settings.py
+++ message_me/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,5 +130,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = '/static/'
