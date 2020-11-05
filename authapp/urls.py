5572d7c10b2bc3c87f24c59f1994eba7abbef1e0
shaked chen <shaked1hen@gmail.com>
2020-11-05 08:54:00 +0200

    get the auth token post view

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include, re_path
 from authapp.views import CreateUserView , ChatConversionView , ChatMessage, ChatMessageUpdate
+from rest_framework.authtoken.views import obtain_auth_token  
 
 urlpatterns = [
-    #path('auth/', include('djoser.urls')),
-    #path('auth/', include('djoser.urls.authtoken')),
-    #path('jwt/', include('djoser.urls')),
-    #path('jwt/', include('djoser.urls.jwt')), # (r'^auth/', include('djoser.urls.jwt'))
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('user/', CreateUserView.as_view(), name='user'),
     path('chatConversion/', ChatConversionView.as_view(), name='chatConversion'),
     re_path(r'^chatMessage/(?P<pk>\d+)/(?P<user_id>\d+)', ChatMessage.as_view(), name='chatMessage'),
